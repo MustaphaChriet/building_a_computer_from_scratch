a79a6fc21cae58b0530b2b8b82ed8df8e6786d72
Mustapha Chriet <mustaphachrietasam@gmail.com>
2023-12-05 08:46:02 +0100

    the first pass of the assembler

--- building assembler/assembler.py
+++ building assembler/assembler.py
@@ -1,3 +1,31 @@
 import sys
+
+# the first pass
+    
+    # symbol table
+symbol_table = {"R0":0, "R1":1, "R2":2, "R3":3, "R4":4, "R5":5, "R6":6, "R7":7, "R8":8, "R9":9, "R10":10, "R11":11, "R12":12, "R13":13, "R14":14, "R15":15, "SCREEN":16384, "KBD":24576, "SP":0, "LCL":1, "ARC":2, "THIS":3, "THAT":4}    
+
 arguments = sys.argv
-print (arguments)
+assembly_file = open(arguments[1], "r")
+    #  ignoring all comments and whide spaces and adding branching symbols the symbol table
+instruction_num = 0    
+for line in assembly_file:
+    line = line.rstrip("\n")
+    n = line.find("//")
+    if n > 0:
+        line = line[:n].strip()
+        if line[0] == "(":
+            symbol_table[line.rstrip(")").lstrip("(")] = instruction_num
+            print (line)
+        else:
+            instruction_num += 1
+            print (line)
+    elif n == -1 and line != "":
+        if line[0] == "(":
+            symbol_table[line.rstrip(")").lstrip("(")] = instruction_num
+            print (line)
+        else:    
+            instruction_num += 1
+            print (line) 
+print (symbol_table)                
+
